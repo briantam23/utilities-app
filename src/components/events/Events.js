25b8bc8ed28d32d661d0d790cf96b5d1067684dc
briantam23 <briantam23@gmail.com>
2019-04-13 15:03:01 -0400

    Readded events folder to change name

---
+++ src/components/events/Events.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+
+class Events extends Component {
+    render() {
+        return(
+            <h1>Ticketmaster Events</h1>
+        )
+    }
+}
+
+
+export default Events;
