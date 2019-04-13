3c2db29e28a4384bd92a8baefca7121bb549fb9e
briantam23 <briantam23@gmail.com>
2019-04-13 14:10:12 -0400

    Minor change

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
-import Stopwatch from './stopwatch/Stopwatch';
-import Events from './events/Events';
+import Stopwatch from './stopwatch/Stopwatch.js';
+import Events from './events/Events.js';
 
 
 const App = () => {
