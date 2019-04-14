146a6ea59fc8b2e9acc5080acdc1918f099fd119
briantam23 <briantam23@gmail.com>
2019-04-13 22:55:35 -0400

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
     devtool: 'source-map',
     output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
+      path: __dirname + '/public',
+      filename: 'bundle.js'
     },
     entry: ['babel-polyfill', './src/index.js'],
     module: {
@@ -15,25 +15,21 @@ module.exports = {
           loader: 'babel-loader'
         },
         {
-            test: /\.less$/,
-            use: [
-              {
-                loader: "style-loader"
-              },
-              {
-                loader: "css-loader",
-                options: {
-                  sourceMap: true,
-                  modules: true,
-                  localIdentName: "[local]___[hash:base64:5]"
-                }
-              },
-              {
-                loader: "less-loader"
+          test: /\.less$/,
+          use: [
+            { loader: "style-loader" },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                modules: true,
+                localIdentName: "[local]___[hash:base64:5]"
               }
-            ]
+            },
+            { loader: "less-loader" }
+          ]
           }
       ]
     },
-    plugins: [ new Dotenv() ]
+    plugins: [new Dotenv({ systemvars: true })]
 };
