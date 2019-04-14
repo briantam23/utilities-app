f1641bdee79627c2dad3cf16649dd750f1563838
briantam23 <briantam23@gmail.com>
2019-04-14 16:56:51 -0400

    Updated app description

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ import style from './home.less';
 const Home = () => (
     <div className={ style.homeContainer }>
         <h1>Brian Tam's Utilities App</h1>
-        <h2>A Single Page App created with React & LESS along with multiple Testing libraries (Enzyme, Mocha, Chai, Sinon, SuperTest).</h2>
+        <h2>A Single Page App created with React, LESS, & Express, along with multiple Testing libraries (Enzyme, Mocha, Chai, Sinon, SuperTest).</h2>
         <img src='../../public/img/profilePicture.jpg' alt='profile picture'/>
     </div>
 )
