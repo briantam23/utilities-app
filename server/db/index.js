b0c451cbe319f2f568f43606ce3eeb1b93280c10
briantam23 <briantam23@gmail.com>
2019-04-15 22:19:10 -0400

    Added seed data

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,14 @@
 const conn = require('./conn');
+const User = require('./models/User');
 
 
 const syncAndSeed = () => {
     conn.sync({ force: true })
+        .then(() => Promise.all([
+            User.create({ name: 'brian', password: 'b' }),
+            User.create({ name: 'mike', password: 'm' }),
+            User.create({ name: 'johnny', password: 'j' })
+        ]))
 }
 
 
