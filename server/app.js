b46460c04138951fb31e024d284319160e2d0211
briantam23 <briantam23@gmail.com>
2019-04-20 15:31:14 -0400

    Added server comments

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
+const volleyball = require('volleyball');
 const path = require('path');
 const { User } = require('./db').models;
 const jwt = require('jwt-simple');
 
 
+// Logging middleware
+app.use(volleyball);
+
+// Body parsing middleware
 app.use(require('body-parser').json());
+
+// Static file-serving middleware
 app.use('/public', express.static(path.join(__dirname, '../public')));
+
+// Sends our index.html (the "single page" of our SPA)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 
 app.use((req, res, next) => {
     const token = req.headers.authorization;
-    if(!token) {
-        return next();
-    }
+    if(!token) return next();
+    
     let id;
     try {
         id = jwt.decode(token, process.env.JWT_SECRET).id;
@@ -28,14 +36,17 @@ app.use((req, res, next) => {
             next();
         })
         .catch(next);
-    
 })
 
+
+// Routes that will be accessed via AJAX that are prepended 
+// with /api so that they are isolated from our GET /* wildcard.
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/todos', require('./routes/todos'));
 
 
+// Error catching endware
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(err.status || 500).send({ error: err.message });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,9 +4,8 @@ const jwt = require('jwt-simple');
 
 
 router.get('/', (req, res, next) => {
-    if(!req.user) {
-        return next({ status: 401 });
-    }
+    if(!req.user) return next({ status: 401 });
+    
     res.send(req.user);
 })
 
@@ -14,9 +13,8 @@ router.post('/', (req, res, next) => {
     const { username, password } = req.body;
     User.findOne({ where: { username, password } })
         .then(user => {
-            if(!user) {
-                return next({ status: 401 });
-            }
+            if(!user) return next({ status: 401 });
+            
             const token = jwt.encode({ id: user.id }, process.env.JWT_SECRET);
             res.send({ token });
         })
