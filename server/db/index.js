e7ef6c077963b72f92da52d89beb2d2e5b6942dc
briantam23 <briantam23@gmail.com>
2019-04-20 12:44:40 -0400

    Added todo seed data

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,6 @@
 const conn = require('./conn');
 const User = require('./models/User');
+const Todo = require('./models/Todo');
 
 
 const syncAndSeed = () => {
@@ -9,13 +10,18 @@ const syncAndSeed = () => {
             User.create({ username: 'Mike', password: 'Mike12#' }),
             User.create({ username: 'Johnny', password: 'Johnny34&' })
         ]))
+        .then(() => Promise.all([
+            Todo.create({ taskName: 'Code', assignee: 'Brian' }),
+            Todo.create({ taskName: 'Take Brazilian Jiu-Jitsu class', assignee: 'Brian' })
+        ]))
 }
 
 
 module.exports = {
     syncAndSeed,
     models: {
-        User
+        User,
+        Todo
     },
     conn
 }
