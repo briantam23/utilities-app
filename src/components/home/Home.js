b16a359413acfa326df45caea596b1545e9bcaad
briantam23 <briantam23@gmail.com>
2019-04-20 14:56:30 -0400

    Updated Homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,7 @@ import style from './home.less';
 const Home = () => (
     <div className={ style.homeContainer }>
         <h1>Brian Tam's Utilities App</h1>
-        <h2>A Single Page App created with React, Redux, LESS, & Express, along with multiple Testing libraries (Enzyme, Mocha, Chai, Sinon, SuperTest).</h2>
+        <h2>A Single Page App created with React, Redux, LESS, & Express, along with multiple Testing libraries (Enzyme, Mocha, Chai, Sinon, SuperTest), and Continuous Integration using TravisCI.</h2>
         <img src='../../public/img/profile_picture.jpg' alt='profile picture'/>
     </div>
 )
