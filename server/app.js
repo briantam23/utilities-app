81286414ba4e53e97d77ed15cf4af51e6c6c47cf
briantam23 <briantam23@gmail.com>
2019-04-20 12:48:30 -0400

    Created Todo routes

--- server/app.js
+++ server/app.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
+app.use('/api/todos', require('./routes/users'));
 
 
 app.use((err, req, res, next) => {
---
+++ server/routes/todos.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const { Todo } = require('../db').models;
+
+
+router.get('/', (req, res, next) => {
+  Todo.findAll()
+    .then(todos => res.json(todos))
+    .catch(next)
+})
+
+router.get('/:todoId', (req, res, next) => {
+  Todo.findByPk(req.params.todoId)
+    .then(todo => res.json(todo))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  Todo.create(req.body)
+    .then(todo => res.json(todo))
+    .catch(next)
+})
+
+router.put('/:todoId', (req, res, next) => {
+  Todo.findByPk(req.params.todoId)
+    .then(todo => todo.update(req.body))
+    .then(todo => res.json(todo))
+    .catch(next)
+})
+
+router.delete('/:todoId', (req, res, next) => {
+  Todo.destroy({
+    where: {
+      id: req.params.todoId
+    }
+  })
+    .then(() => res.status(204).end())
+    .catch(next)
+})
+
+
+module.exports = router;
