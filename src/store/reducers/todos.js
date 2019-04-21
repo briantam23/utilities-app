64bc090820efc031ba1d38734ac35bac09bcfef8
briantam23 <briantam23@gmail.com>
2019-04-20 20:20:12 -0400

    Added more to todosReducer

--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,11 +1,17 @@
 import { LOAD_INITIAL_STUDENTS, CREATE_STUDENT, UPDATE_STUDENT, DESTROY_STUDENT } from '../constants';
-import { LOAD_INITIAL_TODOS } from '../constants';
+import { LOAD_INITIAL_TODOS, CREATE_TODO, UPDATE_TODO, DESTROY_TODO } from '../constants';
 
 
 const todosReducer = (state = [], action) => {
     switch(action.type) {
         case LOAD_INITIAL_TODOS:
             return action.todos;
+        case CREATE_STUDENT:
+            return [...state, action.todos];
+        case UPDATE_STUDENT:
+            return state.map(todo => todo.id !== action.todos.id ? todo : action.todos);
+        case DESTROY_STUDENT:
+            return state.filter(todo => todo.id !== action.todos.id);
         default:
             return state;
     }
