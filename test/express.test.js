5396928b7139dbd534fda16573a3dde277c549a2
briantam23 <briantam23@gmail.com>
2019-04-20 22:57:47 -0400

    Adding more express route testing

--- test/express.test.js
+++ test/express.test.js
@@ -5,6 +5,9 @@ const request = require('supertest');
 const app = require('../server/app');
 const agent = request.agent(app);
 
+const { conn } = require('../server/db');
+const { Todo, User } = require('../server/db').models;
+
 
 describe('The Express Server', () => {
 
@@ -36,6 +39,35 @@ describe('The Express Server', () => {
                 .get('/')
                 .expect('Content-type', 'text/html; charset=UTF-8')
                 .expect(200);
+        })
+    })
+    
+    describe('The `Schools` Route:', () => {
+
+        // First we clear the database before beginning each run
+        before(() => {
+            return conn.sync({ force: true });
+        })
+    
+        // Also, we empty the tables after each spec
+        afterEach(() => {
+            return Promise.all([
+                Todo.truncate({ cascade: true }),
+                User.truncate({ cascade: true })
+            ])
+        })
+    
+        describe('GET /api/todos', () => {
+            it('responds with a array via JSON', async () => {
+    
+                const res = await agent
+                    .get('/api/todos')
+                    .expect('Content-Type', /json/)
+                    .expect(200);
+    
+                expect(res.body).to.be.an.instanceOf(Array);
+                expect(res.body).to.have.length(0);
+            })
         })
     })
 })
