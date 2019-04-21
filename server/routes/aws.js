4e0ff64e5842b9786d7c9930a7ef7b1f4d381c70
briantam23 <briantam23@gmail.com>
2019-04-21 18:31:06 -0400

    Finished AWS route

--- server/routes/aws.js
+++ server/routes/aws.js
@@ -12,3 +12,40 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
+
+// configure AWS to work with promises
+AWS.config.setPromisesDependency(bluebird);
+
+// create S3 instance
+const s3 = new AWS.S3();
+
+// abstracts function to upload a file returning a promise
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: 'public-read',
+    Body: buffer,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  };
+  return s3.upload(params).promise();
+};
+
+// Define POST route
+app.post('/upload', (req, res) => {
+  const form = new multiparty.Form();
+    form.parse(req, async (err, fields, files) => {
+      if (err) throw new Error(err);
+      try {
+        const path = files.file[0].path;
+        const buffer = fs.readFileSync(path);
+        const type = fileType(buffer);
+        const timestamp = Date.now().toString();
+        const fileName = `bucketFolder/${timestamp}-lg`;
+        const data = await uploadFile(buffer, fileName, type);
+        return res.status(200).send(data);
+      } catch (err) {
+        return res.status(400).send(err);
+      }
+    });
+});
