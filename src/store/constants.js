19708a14d6310d4329da790aa483e9e954d736c6
briantam23 <briantam23@gmail.com>
2019-04-20 20:17:12 -0400

    Added Todo constants

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,3 +1,6 @@
 export const SET_AUTH = 'SET_AUTH';
 
 export const LOAD_INITIAL_TODOS = 'LOAD_INITIAL_TODOS';
+export const CREATE_TODO = 'CREATE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const DESTROY_TODO = 'DESTROY_TODO';
