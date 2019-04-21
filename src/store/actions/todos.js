cc60df2abf3ffd759287d12ba1792f67dcda2d61
briantam23 <briantam23@gmail.com>
2019-04-20 20:16:10 -0400

    Added todo Redux actions

--- src/store/actions/todos.js
+++ src/store/actions/todos.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOAD_INITIAL_TODOS } from '../constants';
+import { LOAD_INITIAL_TODOS, CREATE_TODO, UPDATE_TODO, DESTROY_TODO } from '../constants';
 
 
 const _loadInitialTodos = todos => ({
@@ -11,5 +11,45 @@ export const loadInitialTodos = () => (
         axios.get('/api/todos')
             .then(res => res.data)
             .then(todos => dispatch(_loadInitialTodos(todos)))
+    )
+)
+
+const _createTodo = todo => ({
+    type: CREATE_TODO,
+    todos: todo
+})
+export const createTodo = (todo, history) => (
+    dispatch => (
+        axios.post('/api/todos', todo)
+            .then(res => res.data)
+            .then(_todo => dispatch(_createStudent(_todo)))
+            .then(() => history.push('/todos'))
+    )
+)
+
+const _updateTodo = todo => ({
+    type: UPDATE_TODO,
+    todos: todo
+})
+export const updateTodo = (todo, history, redirectToTodos) => (
+    dispatch => (
+        axios.put(`/api/todos/${todo.id}`, todo)
+            .then(res => res.data)
+            .then(_todo => dispatch(_updateTodo(_todo)))
+            .then(() => redirectToTodos ? history.push('/todos') : null)
+    )
+)
+
+const _destroyTodo = todo => ({
+    type: DESTROY_TODO,
+    todos: todo
+})
+export const destroyTodo = (todo, history) => (
+    dispatch => (
+        axios.delete(`/api/todos/${todo.id}`)
+            .then(() => dispatch(_destroyTodo(todo)))
+            .then(() => { 
+                if(history) history.push('/todos'); 
+            })
     )
 )
