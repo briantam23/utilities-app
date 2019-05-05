97e75dd1aca80975a302d012dc6c218fd87ae133
briantam23 <briantam23@gmail.com>
2019-05-05 05:07:17 -0400

    Updated Nav LESS

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -10,8 +10,8 @@ const Nav = () => (
         <li><Link to='/ticketmaster-events'>Events</Link></li>
         <li><Link to='/todos'>Todos</Link></li>
         {/* <li><a href='#'>Air Quality</a></li> */}
-        <li><Link to='/forecast'>Weather Forecast</Link></li>
-        <li><Link to='/my-profile'>My Profile</Link></li>
+        <li><Link to='/forecast'>Weather</Link></li>
+        <li><Link to='/my-profile'>Profile</Link></li>
         {/* <li><a href='#'>Timer</a></li>
         <li><a href='#'>Calculator</a></li>
         <li><a href='#'>World Clock</a></li> */}
