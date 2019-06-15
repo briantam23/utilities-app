3a3b37ad3152953536395a312760fee44e9b2786
briantam23 <briantam23@gmail.com>
2019-06-15 00:01:02 -0400

    Added responsive Nav HTML

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,22 +4,23 @@ import { Link } from 'react-router-dom';
 
 
 const Nav = () => (
-    <div /* className={ style.collapsibleMenu } */>
-        <div>
-            <ul className={ style.nav }>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/stopwatch'>Stopwatch</Link></li>
-                <li><Link to='/ticketmaster-events'>Events</Link></li>
-                <li><Link to='/todos'>Todos</Link></li>
-                {/* <li><a href='#'>Air Quality</a></li> */}
-                <li><Link to='/forecast'>Weather</Link></li>
-                <li><Link to='/my-profile'>Profile</Link></li>
-                {/* <li><a href='#'>Timer</a></li>
+    <header className={ style.header }>
+        {/* <a href="" className={ style.logo }>Nav</a> */}
+        <input className={ style.menuBtn} type="checkbox" id="menu-btn" />
+        <label className={ style.menuIcon} htmlFor="menu-btn"><span className={ style.navicon }></span></label>
+        <ul className={ style.menu }>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/stopwatch'>Stopwatch</Link></li>
+            <li><Link to='/ticketmaster-events'>Events</Link></li>
+            <li><Link to='/todos'>Todos</Link></li>
+            {/* <li><a href='#'>Air Quality</a></li> */}
+            <li><Link to='/forecast'>Weather</Link></li>
+            <li><Link to='/my-profile'>Profile</Link></li>
+            {/* <li><a href='#'>Timer</a></li>
                 <li><a href='#'>Calculator</a></li>
                 <li><a href='#'>World Clock</a></li> */}
-            </ul>
-        </div>
-    </div>
+        </ul>
+    </header>
 )
 
 
