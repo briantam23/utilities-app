75976d24772ac57336d2de324ea4ccfa1b2e3dc5
briantam23 <briantam23@gmail.com>
2019-06-27 08:49:45 -0400

    Added LESS to TodoCreate

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -3,23 +3,25 @@ import { connect } from 'react-redux';
 import { loadInitialTodos } from '../../store/actions/todos';
 import { Route } from 'react-router-dom';
 import style from './todos.less';
+import TodoCreate from './todoCreate/TodoCreate';
 import TodoList from './todoList/TodoList';
-import SingleTodo from './singleTodo/SingleTodo';
 
 
 class Todos extends Component {
     
     componentDidMount = () => {
-        const { loadInitialTodos } = this.props;
-        loadInitialTodos()
+        this.props.loadInitialTodos()
     }
 
     render() {
         return(
-            <div className={ style.todosContainer }>
-                <h1>Todos</h1>
-                <TodoList/>
-            </div>
+            <Fragment>
+                <h1 className={ style.todosHeader }>Todos</h1>
+                <div className={ style.todosContainer }>
+                    <TodoCreate/>
+                    <TodoList/>
+                </div>
+            </Fragment>
         )
     }
 }
--- src/components/todos/todoCreate/TodoCreate.js
+++ src/components/todos/todoCreate/TodoCreate.js
@@ -1,15 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import style from './todoCreate.less';
 
 
 class TodoCreate extends Component {
+
+    state = {
+        taskName: '',
+        assignee: ''
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
+        const { taskName, assignee } = this.state;
+        const { handleChange } = this;
         return(
-            <form>
-                <input name='taskName'/>
-                <input name='assignee'/>
-                <button>Create</button>
-            </form>
+            <div className={ style.todoCreateContainer }>
+                <form className={ style.todoForm }>
+                    <input onChange={ handleChange } value={ taskName } name='taskName'/>
+                    <input onChange={ handleChange } value={ taskName } name='assignee'/>
+                    <button className={ style.todoSubmit }>Create</button>
+                </form>
+            </div>
         )
     }
 }
