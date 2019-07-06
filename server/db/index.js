ef1c5709f44352dd56ea9fa96a33f6780a8d6078
briantam23 <briantam23@gmail.com>
2019-07-06 18:51:55 -0400

    Created Eager Loading routes

--- server/db/index.js
+++ server/db/index.js
@@ -8,17 +8,26 @@ User.hasMany(Todo);
 
 
 const syncAndSeed = () => {
+    let Brian, Mike, Johnny, codeGameApp, takeMuayThaiClass, codeUtilitiesApp, takeBJJClass;
     conn.sync({ force: true })
         .then(() => Promise.all([
             User.create({ username: 'Brian', password: 'Briantam23@' }),
             User.create({ username: 'Mike', password: 'Mike12#' }),
             User.create({ username: 'Johnny', password: 'Johnny34&' })
         ]))
-        .then(() => {
+        .then(users => {
+            [Brian, Mike, Johnny] = users;
             return Promise.all([
-                Todo.create({ taskName: 'Code', assignee: 'Brian' }),
+                Todo.create({ taskName: 'Code a game app', assignee: 'Guest' }),
+                Todo.create({ taskName: 'Take Muay Thai class', assignee: 'Guest' }),
+                Todo.create({ taskName: 'Code Utilities App', assignee: 'Brian' }),
                 Todo.create({ taskName: 'Take Brazilian Jiu-Jitsu class', assignee: 'Brian' })
             ])
+        })
+        .then(todos => {
+            [codeGameApp, takeMuayThaiClass, codeUtilitiesApp, takeBJJClass] = todos;
+            codeUtilitiesApp.setUser(Brian);
+            takeBJJClass.setUser(Brian);
         })
 }
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,22 @@
 const router = require('express').Router();
-const { User } = require('../db').models;
+const { User, Todo } = require('../db').models;
 
 
 //get users
 router.get('/', (req, res, next) => {
-    User.findAll()
+    User.findAll({
+        include: [{ model: Todo }]
+    })
         .then(users => res.send(users))
         .catch(next)
 })
 
 //get user by ID
 router.get('/:userId', (req, res, next) => {
-    User.findByPk(req.params.userId)
+    User.findByPk(
+        req.params.userId, 
+        { include: [{ model: Todo }] }
+    )
         .then(user => res.send(user))
         .catch(next)
 })
