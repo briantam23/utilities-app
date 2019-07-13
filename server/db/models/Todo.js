b474892c2c690d8feaf21d8d6a9d229c613ea714
briantam23 <briantam23@gmail.com>
2019-07-13 07:12:29 -0400

    Updated hooks

--- server/db/models/Todo.js
+++ server/db/models/Todo.js
@@ -2,26 +2,31 @@ const conn = require('../conn');
 const _ = require('lodash');
 
 
-const Todo = conn.define('todos', {
-    taskName: {
-        type: conn.Sequelize.STRING,
-        allowNull: false,
-        validate: { notEmpty: true }
+const Todo = conn.define('todos', 
+    {
+        taskName: {
+            type: conn.Sequelize.STRING,
+            allowNull: false,
+            validate: { notEmpty: true }
+        },
+        assignee: {
+            type: conn.Sequelize.STRING,
+            allowNull: false
+        }
     },
-    assignee: {
-        type: conn.Sequelize.STRING,
-        allowNull: false
+    {
+        hooks: {
+            beforeCreate: (todo, options) => todoHook(todo),
+            beforeUpdate: (todo, options) => todoHook(todo)
+        }
     }
-})
+)
 
 const todoHook = todo => {
     const { assignee } = todo;
     //todo.assignee = assignee.charAt(0).toUpperCase() + assignee.slice(1);
     todo.assignee = _.capitalize(assignee);
 }
-
-Todo.beforeCreate((todo, options) => todoHook(todo));
-Todo.beforeUpdate((todo, options) => todoHook(todo));
 
 
 module.exports = Todo;
